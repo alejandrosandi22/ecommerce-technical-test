5363eb53012da8f4843dbaf754340109a0e33cfb
Alejandro Sandí <alesf22@gmail.com>
2025-06-12 11:22:15 -0600

    feat: configure route middleware

--- backend/server.js
+++ backend/server.js
@@ -10,5 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 const db = require('./models');
 db.sequelize.sync();
 
+require('./routes/auth.routes')(app);
+require('./routes/product.routes')(app);
+require('./routes/user.routes')(app);
+require('./routes/cart.routes')(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
