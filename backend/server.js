ab0474422f847f660eaadba121fdaa41e4fbf1d2
Alejandro Sandí <alesf22@gmail.com>
2025-06-13 13:47:57 -0600

    build: update dependencies and server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const db = require('./models');
 db.sequelize.sync();
